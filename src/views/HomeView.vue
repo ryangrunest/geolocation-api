2af039b02657a0860a088d657dbab58cea3eb67b
ryan.grunest <ryan.grunest@chipotle.com>
2023-01-20 10:48:31 -0800

    commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      lat: '',
+      lng: ''
+    }
+  },
+  methods: {
+    onGeolocationError(err) {
+      console.warn(`ERROR: ${err.code}: ${err.message}`);
+    },
+    onGeolocationSuccess(pos) {
+      console.log('got cords', pos);
+
+      this.lat = pos.coords.latitude;
+      this.lng = pos.coords.longitude;
+    },
+    requestGeolocation() {
+      console.log('adf')
+      navigator.geolocation.getCurrentPosition(this.onGeolocationSuccess, this.onGeolocationError)
+        
+    },
+  },
+  mounted() {
+    this.requestGeolocation();
+  }
+}
+</script>
+
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <p>lat: {{lat}}</p>
+    <p>long: {{lng}}</p>
+    <button @click="requestGeolocation">request geolocation</button>
+  </main>
+</template>
